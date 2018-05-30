40e02e083f9ec53deafe940ef29f2a0910eec279
ledet <ledet2016@gmail.com>
2018-05-30 13:25:40 -0500

    testing the merge

--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,54 @@
 //grab provided args.
 // const [,, ...args] = process.argv
+
+// merge: regex  grep checkout changes for both // git log --merges -l
+// checkout: parsing grep checkout changes for both
+
 import * as yargs from 'yargs';
 import * as shell from 'shelljs';
 import colors from './colors';
+import { inferGitCommand } from './util';
 
-// const args = yargs.argv;
-const args = process.argv.slice(2);
+let args = String(yargs.argv.git_params);
+// const args = process.argv.slice(2);
+console.log(typeof args);
 
-console.log('the arges are ', args);
+let type = inferGitCommand(args);
+console.log(type);
+
+
+ const parsedArgs = args.split(" ");
+
+ console.log(parsedArgs);
+
+
 //print args
-console.log('hash1='+ args[0]);
-console.log('hash2='+ args[1]);
-console.log("show log="+ args[2]);
+console.log('hash1='+ parsedArgs[0]);
+console.log('hash2='+ parsedArgs[1]);
+console.log("show log="+ parsedArgs[2]);
 
-if(args[2] === '1') {
+
+if(parsedArgs[2] === '1') {
   console.log(colors.bg.Blue, colors.fg.White, "GIT LOG", colors.Reset);
   const result = shell.exec(`git --no-pager log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative -n 15 --color=always`, {silent:true}).stdout;
   console.log(result);
   console.log("\n");
+  
 
 }
 
-if(args[0] !== args[1]){
-  const result = shell.exec(`git --no-pager log ${args[1]}..${args[0]} --grep 'setup repository to being working' | grep 'setup repository to being working'`, {silent:true}).stdout;
+if(parsedArgs[0] == parsedArgs[1]){
+  const result = shell.exec(`git --no-pager log ${parsedArgs[1]}..${parsedArgs[0]} --grep 'setup repository to being working' | grep 'setup repository to being working'`, {silent:true}).stdout;
     
   console.log(colors.bg.Red, colors.fg.White, 'BREAKING CHANGES', colors.Reset);
   console.log(colors.bg.Red, colors.fg.White, 'Below are the list of breaking changes:', colors.Reset);
   console.log(colors.fg.Red,`${result}`, colors.Reset);
-  console.log("test");
+  
 }
+
+if (type === 'merge') {
+  console.log("the type is a merge");
+  const gitRepo = shell.exec(`git --no-pager log --merges -1`);
+  console.log(gitRepo);
+}
+
--- src/util.ts
+++ src/util.ts
@@ -3,7 +3,17 @@
  *  post-merge - single parameter, a status flag, true if a squash merge
  *  post-checkout - three params - ref of prev, ref of new, true if new branch
  */
+let hasWhitespace = (input: string) :boolean=> {
+  return input.includes(' ');
+}
+
+
 export const inferGitCommand = (parameters: string) => {
+
+  if (!hasWhitespace(parameters)) {
+    return 'merge';
+  }
+
   const paramsArray = parameters.split(' ');
   
   switch (paramsArray.length) {
@@ -12,3 +22,14 @@ export const inferGitCommand = (parameters: string) => {
     default: return undefined;
   }
 };
+
+export const inferGitCommandForArr = (params: string[]) => {
+
+  switch (params.length) {
+    case 1: return 'merge';
+    case 3: return 'checkout';
+    default: return undefined;
+  }
+}
+
+
